5d9a87bbfeb9145c40347f5ea2654308296e7209
chinkhor <chin.kooi.khor@gmail.com>
2020-06-11 23:30:25 +0800

    Added pieces to boards

--- HW4/src/Checker.java
+++ HW4/src/Checker.java
@@ -1,9 +1,12 @@
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -14,28 +17,56 @@ public class Checker {
 		JFrame frame = new JFrame();
 		frame.setTitle("Checker");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400, 400);
+		frame.setSize(800, 800);
 		
 		Container pane = frame.getContentPane();
         pane.setLayout(new GridLayout(8,8));
         
+        JPanel[][] panel = new JPanel[8][8];
         int count = 0;
         for (int i=0; i<8; i++)
         {
         	count++;
   			for (int j=0; j<8; j++)
   			{
-  				JPanel panel = new JPanel();
   				count++;
+  				panel[i][j] = new JPanel();
   				if (count%2!=0)
-  					panel.setBackground(Color.RED);
+  					panel[i][j].setBackground(Color.RED);
   				else 
-  					panel.setBackground(Color.BLACK);
-  				panel.setPreferredSize(new Dimension(50,50));
-  				pane.add(panel);
+  					panel[i][j].setBackground(Color.BLACK);
+  				panel[i][j].setPreferredSize(new Dimension(100,100));
+  				panel[i][j].setLayout(new BorderLayout());
+  				pane.add(panel[i][j]);
+  			}
+        }
+        
+        ArrayList<CheckerComponent> playerW = new ArrayList<CheckerComponent>();
+        for (int i=0; i<3; i++)
+        {
+        	int j = i % 2;
+        	while (j < 8)
+  			{
+        		CheckerComponent piece = new CheckerComponent(Color.WHITE,i,j);
+        		panel[i][j].add(piece, BorderLayout.CENTER);
+        		playerW.add(piece);
+  				j+=2;
   			}
         }
 		
+        ArrayList<CheckerComponent> playerY = new ArrayList<CheckerComponent>();
+        for (int i=5; i<8; i++)
+        {
+        	int j = i % 2;
+        	while (j < 8)
+  			{
+        		CheckerComponent piece = new CheckerComponent(Color.ORANGE,i,j);
+        		panel[i][j].add(piece, BorderLayout.CENTER);
+        		playerY.add(piece);
+  				j+=2;
+  			}
+        }
+        
 		frame.setVisible(true);
 	}
 
---
+++ HW4/src/CheckerComponent.java
@@ -0,0 +1,30 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JButton;
+
+public class CheckerComponent extends JButton
+{  
+   private int x = 25, y = 25;
+   private int size = 50;
+   private Color player;
+   private int row, col;
+   
+   public CheckerComponent(Color player, int row, int col)
+   {
+	   super();
+	   setBorderPainted(false);
+	   this.player = player;
+	   this.row = row;
+	   this.col = col;
+   }
+   
+   public void paintComponent(Graphics g)
+   {  
+      g.setColor(player);
+      g.drawOval(x, y, size, size);
+      g.fillOval(x, y, size, size);
+      super.paintComponent(g);
+   }
+}
